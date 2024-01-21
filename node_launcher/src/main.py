1bbc4fb2d6853787188a69ff7cd86a52b964b1be
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2024-01-21 14:41:10 +0200

    used docker instead of subprocess + implemented basic rest api with flask

--- node_launcher/src/main.py
+++ node_launcher/src/main.py
@@ -1,13 +1,30 @@
 import xml.etree.ElementTree as ET
-import subprocess
+import docker
 from flask import Flask
+
+PREFIX = "dht"
+DOCKER_CLIENT = docker.from_env()
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/run_all")
+def run_all():
+    # TODO:
+    # 1. fetch config stored in baseX
+    # OR
+    # 2. receive it as parameter
+    # 1. looks better
+    machines = parse_xml_config()
+
+    run_containers(machines)
+    return "ok"
+
+
+@app.route("/delete_all")
+def delete_all():
+    delete_all_containers()
+    return "ok"
 
 
 def parse_xml_config() -> list['Machine']:
@@ -30,17 +47,40 @@ def parse_xml_config() -> list['Machine']:
     return machines
 
 
-def launch_machines(machines: list['Machine']):
+def run_containers(machines: list['Machine']):
     for machine in machines:
-        command = f"docker run --network='host' \
-        --env secret_key_seed={machine.seed} --datas "
+        machine_name = PREFIX + machine.seed
 
-        for key, value in machine.datas:
-            command += f"{key}={value}"
+        data = ""
 
-        command += " dht-core:latest"
+        for key, value in machine.datas.items():
+            data += f"{key}={value};"
 
-        subprocess.run(command)
+        env = {"secret_key_seed": machine.seed, "data": data}
+
+        DOCKER_CLIENT.containers.run(
+            image="dht-core",
+            detach=True,  # -d
+            environment=env,
+            name=machine_name,
+            # ports={62500: 62500},
+            network="host",
+        )
+
+        print(f"Container {machine_name} started")
+
+
+def delete_all_containers():
+    containers = DOCKER_CLIENT.containers.list(all=True)
+
+    for container in containers:
+        if container.name.startswith(PREFIX):
+            if container.status == "running":
+                container.stop()
+                print(f"Container {container.name} stopped")
+
+            container.remove()
+            print(f"Container {container.name} deleted")
 
 
 class Machine:
